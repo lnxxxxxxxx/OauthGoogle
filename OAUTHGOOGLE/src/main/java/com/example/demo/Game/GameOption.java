1d867e52be1d3cf45020f5ab7604a8c11b46e662
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-03-30 13:37:34 -0300

    practico pruebas junit y mockito

---
+++ OAUTHGOOGLE/src/main/java/com/example/demo/Game/GameOption.java
@@ -0,0 +1,5 @@
+package com.example.demo.Game;
+
+public enum GameOption {
+    ROCK, PAPER, SCISSORS;
+}
---
+++ OAUTHGOOGLE/src/main/java/com/example/demo/Moneyt/Money.java
@@ -0,0 +1,8 @@
+package com.example.demo.Moneyt;
+
+public class Money {
+    public static void main(String[] args) {
+        double salary = 1000;
+        System.out.println(MoneyUtil.format(salary));
+    }
+}
